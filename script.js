cbf3e8e0f6bbe8c2667cd8303d5826fbcb40f0a7
KarnaVidyaRatnaSri <137883417+KarnaVidyaRatnaSri@users.noreply.github.com>
2023-07-19 10:31:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function summer(){
+    document.getElementById("season").src="https://previews.123rf.com/images/zhaojiankangphoto/zhaojiankangphoto1908/zhaojiankangphoto190800324/128748794-green-grass-and-blue-sky-with-white-clouds-in-summer-season.jpg";
+    
+}
+function rainy(){
+    document.getElementById("season").src="https://images.unsplash.com/photo-1619260584294-8a4e63f5ade5?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmFpbnklMjBzZWFzb258ZW58MHx8MHx8fDA%3D&w=1000&q=80";
+
+}
+function winter(){
+    document.getElementById("season").src="https://cdn.xxl.thumbs.canstockphoto.com/a-winter-season-illustration-of-a-winter-season-vector-clip-art_csp15039015.jpg";
+}
